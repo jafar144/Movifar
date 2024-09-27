fe39b6c042f278ff71a3f3494cbecc6a38494ec4
JafarAssegaf <ajib.aiwa@gmail.com>
2024-09-27 22:20:01 +0700

    Fix Dynamic Feature Error (Add dynamic feature navigation fragment)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.activity)
+    implementation(libs.androidx.navigation.dynamic.features.fragment)
 
     // Lottie Animation
     implementation(libs.lottie)
