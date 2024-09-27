5c78e8381a1077f2e17ba9ab858b33af13fc2e0b
JafarAssegaf <ajib.aiwa@gmail.com>
2024-09-27 20:59:37 +0700

    Change gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,14 +30,6 @@ android {
                 "proguard-rules.pro"
             )
         }
-
-        debug {
-            isMinifyEnabled = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
